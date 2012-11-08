f14b9d33b0b5d84ecc52a8a002d9d7fb74e94e3b
Ewerton <ewerton.sc@gmail.com>
2012-11-07 22:44:07 -0200

    Adding some comments

--- Player.cs
+++ Player.cs
@@ -18,6 +18,7 @@ namespace ShapeGame
 
     public class Player
     {
+        // Here is defined the size of the ellipse that represent each element (bones, Head, hands)
         private const double BoneSize = 0.01;
         private const double HeadSize = 0.075;
         private const double HandSize = 0.03;
@@ -28,8 +29,10 @@ namespace ShapeGame
         private readonly System.Windows.Media.Brush bonesBrush;
         private readonly int id;
         private static int colorId;
-        private Rect playerBounds;
-        private System.Windows.Point playerCenter;
+        private Rect playerBounds; // ???
+        private System.Windows.Point playerCenter; //???
+
+        //??? I am expection that it render diferent persons (adults and childrens) ins same size, but it actually doesnt do it
         private double playerScale;
 
         public Player(int skeletonSlot)
@@ -68,31 +71,35 @@ namespace ShapeGame
             return this.id;
         }
 
+        // ??? 
         public void SetBounds(Rect r)
         {
             this.playerBounds = r;
             this.playerCenter.X = (this.playerBounds.Left + this.playerBounds.Right) / 2;
             this.playerCenter.Y = (this.playerBounds.Top + this.playerBounds.Bottom) / 2;
+
+            // ??? whats player scale means?
             this.playerScale = Math.Min(this.playerBounds.Width, this.playerBounds.Height / 2);
         }
 
         public void UpdateBonePosition(Microsoft.Kinect.JointCollection joints, JointType j1, JointType j2)
         {
+
+            //??? what this do?
             var seg = new Segment(
                 (joints[j1].Position.X * this.playerScale) + this.playerCenter.X,
                 this.playerCenter.Y - (joints[j1].Position.Y * this.playerScale),
                 (joints[j2].Position.X * this.playerScale) + this.playerCenter.X,
-                this.playerCenter.Y - (joints[j2].Position.Y * this.playerScale))
-                { Radius = Math.Max(3.0, this.playerBounds.Height * BoneSize) / 2 };
+                this.playerCenter.Y - (joints[j2].Position.Y * this.playerScale)) { Radius = Math.Max(3.0, this.playerBounds.Height * BoneSize) / 2 };
             this.UpdateSegmentPosition(j1, j2, seg);
         }
 
         public void UpdateJointPosition(Microsoft.Kinect.JointCollection joints, JointType j)
         {
+            //??? what this do?
             var seg = new Segment(
                 (joints[j].Position.X * this.playerScale) + this.playerCenter.X,
-                this.playerCenter.Y - (joints[j].Position.Y * this.playerScale))
-                { Radius = this.playerBounds.Height * ((j == JointType.Head) ? HeadSize : HandSize) / 2 };
+                this.playerCenter.Y - (joints[j].Position.Y * this.playerScale)) { Radius = this.playerBounds.Height * ((j == JointType.Head) ? HeadSize : HandSize) / 2 };
             this.UpdateSegmentPosition(j, j, seg);
         }
 
@@ -147,6 +154,7 @@ namespace ShapeGame
             }
         }
 
+        //??? Whats this do?
         private void UpdateSegmentPosition(JointType j1, JointType j2, Segment seg)
         {
             var bone = new Bone(j1, j2);
@@ -162,4 +170,99 @@ namespace ShapeGame
             }
         }
     }
+
+
+    #region "Copied from FallingShapes class, just for knowing"
+
+    // For hit testing, a dictionary of BoneData items, keyed off the endpoints
+    // of a segment (Bone) is used.  The velocity of these endpoints is estimated
+    // and used during hit testing and updating velocity vectors after a hit.
+    public struct Bone
+    {
+        public JointType Joint1;
+        public JointType Joint2;
+
+        public Bone(JointType j1, JointType j2)
+        {
+            this.Joint1 = j1;
+            this.Joint2 = j2;
+        }
+    }
+
+
+    public struct BoneData
+    {
+        public Segment Segment;
+        public Segment LastSegment;
+        public double XVelocity;
+        public double YVelocity;
+        public double XVelocity2;
+        public double YVelocity2;
+        public DateTime TimeLastUpdated;
+
+        private const double Smoothing = 0.8;
+
+        public BoneData(Segment s)
+        {
+            this.Segment = this.LastSegment = s;
+            this.XVelocity = this.YVelocity = 0;
+            this.XVelocity2 = this.YVelocity2 = 0;
+            this.TimeLastUpdated = DateTime.Now;
+        }
+
+        // Update the segment's position and compute a smoothed velocity for the circle or the
+        // endpoints of the segment based on  the time it took it to move from the last position
+        // to the current one.  The velocity is in pixels per second.
+        public void UpdateSegment(Segment s)
+        {
+            this.LastSegment = this.Segment;
+            this.Segment = s;
+
+            DateTime cur = DateTime.Now;
+            double fMs = cur.Subtract(this.TimeLastUpdated).TotalMilliseconds;
+            if (fMs < 10.0)
+            {
+                fMs = 10.0;
+            }
+
+            double fps = 1000.0 / fMs;
+            this.TimeLastUpdated = cur;
+
+            if (this.Segment.IsCircle())
+            {
+                this.XVelocity = (this.XVelocity * Smoothing) + ((1.0 - Smoothing) * (this.Segment.X1 - this.LastSegment.X1) * fps);
+                this.YVelocity = (this.YVelocity * Smoothing) + ((1.0 - Smoothing) * (this.Segment.Y1 - this.LastSegment.Y1) * fps);
+            }
+            else
+            {
+                this.XVelocity = (this.XVelocity * Smoothing) + ((1.0 - Smoothing) * (this.Segment.X1 - this.LastSegment.X1) * fps);
+                this.YVelocity = (this.YVelocity * Smoothing) + ((1.0 - Smoothing) * (this.Segment.Y1 - this.LastSegment.Y1) * fps);
+                this.XVelocity2 = (this.XVelocity2 * Smoothing) + ((1.0 - Smoothing) * (this.Segment.X2 - this.LastSegment.X2) * fps);
+                this.YVelocity2 = (this.YVelocity2 * Smoothing) + ((1.0 - Smoothing) * (this.Segment.Y2 - this.LastSegment.Y2) * fps);
+            }
+        }
+
+        // Using the velocity calculated above, estimate where the segment is right now.
+        public Segment GetEstimatedSegment(DateTime cur)
+        {
+            Segment estimate = this.Segment;
+            double fMs = cur.Subtract(this.TimeLastUpdated).TotalMilliseconds;
+            estimate.X1 += fMs * this.XVelocity / 1000.0;
+            estimate.Y1 += fMs * this.YVelocity / 1000.0;
+            if (this.Segment.IsCircle())
+            {
+                estimate.X2 = estimate.X1;
+                estimate.Y2 = estimate.Y1;
+            }
+            else
+            {
+                estimate.X2 += fMs * this.XVelocity2 / 1000.0;
+                estimate.Y2 += fMs * this.YVelocity2 / 1000.0;
+            }
+
+            return estimate;
+        }
+    }
+
+    #endregion
 }
